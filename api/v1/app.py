4097ebab30caddd5fccdfbc12de443feed43db4f
Yousef eslam <yousefeslam214@gmail.com>
2024-06-08 13:18:12 +0300

    12-task

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-cors = CORS(app, resource={r"/api/*": {"origins": "0.0.0.0"}})
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
